9bf0eee86cc072be4a6c1a48a1c15c024b76a4b5
Eugeni S <esenent@hotmail.com>
2016-04-13 09:06:12 +0200

    Change Data Model to support disc image

--- app/src/main/java/apps/xenione/com/swipelayout/data/Album.java
+++ app/src/main/java/apps/xenione/com/swipelayout/data/Album.java
@@ -3,6 +3,8 @@ package apps.xenione.com.swipelayout.data;
 import java.util.ArrayList;
 import java.util.List;
 
+import apps.xenione.com.swipelayout.R;
+
 /**
  * Created by Eugeni on 10/04/2016.
  */
@@ -11,50 +13,48 @@ public class Album {
     public static List<Album> getAlbum() {
 
         return new ArrayList<Album>() {{
-            add(new Album("La Gossa Sorda", "Saó", 2008));
-            add(new Album("ZOO", "Tempestes vénen del sud", 2014));
-            add(new Album("Auxili", "Dolç Atac", 2013));
-            add(new Album("La Raíz", "Así en el cielo como en la selva", 2013));
-            add(new Album("Aspencat", "Essència", 2013));
-            add(new Album("Bajoqueta rock", "A Pèl ", 1998));
-            add(new Album("Diluvi", "Rareses i cançons soltes ", 2013));
+            add(new Album(1, "La Gossa Sorda", "Saó", 2008, R.drawable.disc_gossa_sorda_sao));
+            add(new Album(2, "ZOO", "Tempestes vénen del sud", 2014, R.drawable.disc_zoo_tempestes));
+            add(new Album(3, "Auxili", "Dolç Atac", 2013, R.drawable.disc_auxili_dolc));
+            add(new Album(4, "La Raíz", "Así en el cielo como en la selva", 2013, R.drawable.disc_raiz_asi_en));
+            add(new Album(5, "Aspencat", "Essència", 2013, R.drawable.disc_aspencat_essencia));
+            add(new Album(6, "Bajoqueta rock", "A Pèl ", 1998, R.drawable.disc_bajoqueta_apel));
+            add(new Album(7, "Diluvi", "Motius", 2013, R.drawable.disc_diluvi_motius));
         }};
-
     }
 
+    public long id;
     public String name;
-
     public String bandName;
-
     public int releaseYear;
+    public int resId;
 
-    public Album(String bandName,String name, int releaseYear) {
+    public Album(long id, String bandName, String name, int releaseYear, int resId) {
+        this.id = id;
         this.bandName = bandName;
         this.name = name;
         this.releaseYear = releaseYear;
+        this.resId = resId;
+    }
+
+    public long getId(){
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getBandName() {
         return bandName;
-    }
-
-    public void setBandName(String bandName) {
-        this.bandName = bandName;
     }
 
     public int getReleaseYear() {
         return releaseYear;
     }
 
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
+    public int getResource(){
+        return resId;
     }
+
 }
